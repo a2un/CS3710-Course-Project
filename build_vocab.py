3c920115d29d9aaf7a87155b1ba606a2bf94df30
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-24 22:29:37 -0400

    sem embedding

--- build_vocab.py
+++ build_vocab.py
@@ -19,17 +19,17 @@ def build_dictionary(texts, vocab_size, lexical, syntactic, semantic):
     word2idx = {word: idx for idx, word in enumerate(words)}
 
 
-    # sem_embedding = None
+    sem_embedding = None
     # syn_embedding = None
-    # with tf.Session() as sess:
-    #     saver = tf.train.Saver()
+    with tf.Session() as sess:
+        saver = tf.train.Saver()
     #     saver.restore('./tools/syntactic_semantic_embeddings/embeddings/syntactic_embeddings')
     #     syn_embedding = sess.run()
     #     syn_embedding = tf.convert_to_tensor(syn_embedding)
-    #     saver.restore('./tools/syntactic_semantic_embeddings/embeddings/semantic_embeddings')
-    #     sem_embedding = sess.run()
-    #     sem_embedding = tf.convert_to_tensor(sem_embedding)
+        saver.restore('./tools/syntactic_semantic_embeddings/embeddings/semantic_embeddings')
+        sem_embedding = sess.run()
+        sem_embedding = tf.convert_to_tensor(sem_embedding)
     # print('syn embedding size',syn_embedding.size())
-    # print('sem embedding size',sem_embedding.size())
+    print('sem embedding size',sem_embedding.size())
 
     return word2idx, GloVe(name='6B')
