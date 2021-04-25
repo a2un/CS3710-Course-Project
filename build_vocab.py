8b67972cf3b6ff7cd5e13cd4b56a31f96ff3e296
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-25 00:39:49 -0400

    red

--- build_vocab.py
+++ build_vocab.py
@@ -22,8 +22,8 @@ def build_dictionary(texts, vocab_size, lexical, syntactic, semantic):
     words = words if lexical else SPECIAL_TOKENS + words
     word2idx = {word: idx for idx, word in enumerate(words)}
 
-    sem_embed = torch.from_numpy(np.array([[float(eval(score,{},{})) if bool(re.match(r'^-?\d+(\.\d+)?$', score)) else 0 for score in line.split()[1:]] for line in open(sem_embed_path,'r').readlines()]))
-    syn_embed = torch.from_numpy(np.array([[float(eval(score,{},{})) if bool(re.match(r'^-?\d+(\.\d+)?$', score)) else 0 for score in line.split()[1:]] for line in open(syn_embed_path,'r').readlines()]))
+    sem_embed = torch.from_numpy(np.array([[float(eval(score,{},{})) if bool(re.match(r'^-?\d+(\.\d+)?$', score)) else 0 for score in line.split()[1:]] for line in open(sem_embed_path,'r').readlines()[:300]]))
+    syn_embed = torch.from_numpy(np.array([[float(eval(score,{},{})) if bool(re.match(r'^-?\d+(\.\d+)?$', score)) else 0 for score in line.split()[1:]] for line in open(syn_embed_path,'r').readlines()[:300]]))
 
     print('syn embedding size',syn_embedding.size())
     print('sem embedding size',sem_embedding.size())
