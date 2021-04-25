890142f2c4e11a963f9ff619580b4178884f490b
Arun Balajiee Lekshmi Narayanan <ARL122@pitt.edu>
2021-04-25 00:38:18 -0400

    syn

--- build_vocab.py
+++ build_vocab.py
@@ -23,7 +23,7 @@ def build_dictionary(texts, vocab_size, lexical, syntactic, semantic):
     word2idx = {word: idx for idx, word in enumerate(words)}
 
     sem_embed = torch.from_numpy(np.array([[float(eval(score,{},{})) if bool(re.match(r'^-?\d+(\.\d+)?$', score)) else 0 for score in line.split()[1:]] for line in open(sem_embed_path,'r').readlines()]))
-    syn_embed = torch.from_numpy(np.array([[float(eval(score,{},{})) if bool(re.match(r'^-?\d+(\.\d+)?$', score)) else 0 for score in line.split()[1:]] for line in open(sem_embed_path,'r').readlines()]))
+    syn_embed = torch.from_numpy(np.array([[float(eval(score,{},{})) if bool(re.match(r'^-?\d+(\.\d+)?$', score)) else 0 for score in line.split()[1:]] for line in open(syn_embed_path,'r').readlines()]))
 
     print('syn embedding size',syn_embedding.size())
     print('sem embedding size',sem_embedding.size())
